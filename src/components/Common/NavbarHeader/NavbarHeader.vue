45b408cc4eaa29b3795e54028fe9eeb395233612
Kamolxon <kamolxonizzatullayev2001@gmail.com>
2022-10-04 18:55:23 +0500

    added home section

---
+++ src/components/Common/NavbarHeader/NavbarHeader.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="c-cu-navbar">
+    <div class="container">
+      <div class="navbar-header d-flex justify-content-end">
+        <nav>
+          <ul class="navbar-header__list">
+            <li class="navbar-header__list-item">
+              <a
+                class="navbar-header__list-item-link d-flex align-items-center"
+                href="#"
+              >
+                <img src="@/assets/icons/user.svg" alt="user" />
+                <span>Авторизоваться </span></a
+              >
+            </li>
+            <li class="navbar-header__list-item">
+              <a
+                class="navbar-header__list-item-link d-flex align-items-center"
+                href="#"
+              >
+                <img src="@/assets/icons/nastroyka.svg" alt="nastroyka" />
+                <span>Справочник</span>
+              </a>
+            </li>
+            <li class="navbar-header__list-item">
+              <a
+                class="navbar-header__list-item-link d-flex align-items-center"
+                href="#"
+              >
+                <img src="@/assets/icons/lang.svg" alt="nastroyka" />
+                <span>Руcский</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/styles/index.scss";
+@import "./NavbarHeader.style.scss";
+</style>
---
+++ src/components/Layout/HeaderView/HeaderView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="c-l-header">
+    <div class="bg"></div>
+
+    <NavbarHeader></NavbarHeader>
+
+    <MenuNavbar></MenuNavbar>
+
+    <h1>Oт желания к реальности</h1>
+
+    <SearchField></SearchField>
+  </div>
+</template>
+
+<script>
+import MenuNavbar from "@/components/Common/MenuNavbar/MenuNavbar.vue";
+import NavbarHeader from "@/components/Common/NavbarHeader/NavbarHeader.vue";
+import SearchField from "@/components/Custom/SearchField/SearchField.vue";
+
+export default {
+  components: {
+    MenuNavbar,
+    NavbarHeader,
+    SearchField
+},
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/styles/index.scss";
+@import "./HeaderView.style.scss";
+</style>
